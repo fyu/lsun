25008ead6254aef5f2bf4cb38f70073be0853cbe
Fisher Yu <fy@cs.princeton.edu>
2015-05-11 13:58:53 -0700

    update doc

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Data Release
 
 All the images in one category are stored in one lmdb database file. The value
- of each entry is the jpg binary data. We resize all the images so that the 
+ of each entry is the jpg binary data. We resize all the images so that the
  smaller dimension is 256 and compress the images in jpeg with quality 75.
 
 ## Demo code
@@ -19,11 +19,20 @@ Install Python dependency: numpy, lmdb, opencv
 View the lmdb content
 
 ```bash
-python data.py view <image db path>
+python2.7 data.py view <image db path>
 ```
 
 Export the images to a folder
 
 ```bash
-python data.py export <image db path> --out_dir <output directory>
+python2.7 data.py export <image db path> --out_dir <output directory>
+```
+
+### Example:
+
+Export all the images in valuation sets in the current folder to a "data"
+subfolder.
+
+```bash
+python2.7 data.py export *_val_lmdb --out_dir data
 ```
--- data.py
+++ data.py
@@ -14,6 +14,8 @@ __license__ = 'MIT'
 
 
 def view(db_path):
+    print('Viewing', db_path)
+    print('Press ESC to exist or SPACE to advance.')
     window_name = 'LSUN'
     cv2.namedWindow(window_name)
     env = lmdb.open(db_path, map_size=1099511627776,
@@ -21,8 +23,7 @@ def view(db_path):
     with env.begin(write=False) as txn:
         cursor = txn.cursor()
         for key, val in cursor:
-            print(key)
-            print('Press ESC to exist or SPACE to advance.')
+            print('Current key:', key)
             img = cv2.imdecode(
                 numpy.fromstring(val, dtype=numpy.uint8),
                 cv2.CV_LOAD_IMAGE_COLOR)
@@ -33,8 +34,10 @@ def view(db_path):
 
 
 def export_images(db_path, out_dir):
+    print('Exporting', db_path, 'to', out_dir)
     env = lmdb.open(db_path, map_size=1099511627776,
                     max_readers=100, readonly=True)
+    count = 0
     with env.begin(write=False) as txn:
         cursor = txn.cursor()
         for key, val in cursor:
@@ -44,23 +47,34 @@ def export_images(db_path, out_dir):
             image_out_path = join(image_out_dir, key + '.jpg')
             with open(image_out_path, 'w') as fp:
                 fp.write(val)
+            count += 1
+            if count % 100 == 0:
+                print('Finished', count, 'images')
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('command', nargs=1, type=str,
-                        choices=['view', 'export'])
-    parser.add_argument('lmdb_path', nargs=1, type=str)
+    parser.add_argument('command', nargs='?', type=str,
+                        choices=['view', 'export'],
+                        help='view: view the images in the lmdb database '
+                             'interactively.\n'
+                             'export: Export the images in the lmdb databases '
+                             'to a folder. The images are grouped in subfolders'
+                             ' determinted by the prefiex of image key.')
+    parser.add_argument('lmdb_path', nargs='+', type=str,
+                        help='The path to the lmdb database folder. '
+                             'Support multiple database paths.')
     parser.add_argument('--out_dir', type=str, default='')
     args = parser.parse_args()
 
-    command = args.command[0]
-    lmdb_path = args.lmdb_path[0]
+    command = args.command
+    lmdb_paths = args.lmdb_path
 
-    if command == 'view':
-        view(lmdb_path)
-    elif command == 'export':
-        export_images(lmdb_path, args.out_dir)
+    for lmdb_path in lmdb_paths:
+        if command == 'view':
+            view(lmdb_path)
+        elif command == 'export':
+            export_images(lmdb_path, args.out_dir)
 
 
 if __name__ == '__main__':
